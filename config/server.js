9f8f51456a11adc139fce416832a4f17616696e7
Tovy Vaits <antonvaits@gmail.com>
2022-12-12 00:11:44 +0200

    edit env

--- config/server.js
+++ config/server.js
@@ -1,12 +1,9 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+    app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  },
   url: 'http://188.120.234.3/',
+  proxy: true,
 });
