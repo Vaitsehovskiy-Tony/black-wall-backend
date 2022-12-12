9c5756537648e2e95cb8b8f70c3717969f743446
Tovy Vaits <antonvaits@gmail.com>
2022-12-12 13:36:09 +0200

    setting token

--- config/server.js
+++ config/server.js
@@ -1,7 +1,5 @@
-const http = require("http");
-
 module.exports = ({ env }) => ({
-  host: env('HOST', '188.120.234.3'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
     app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
