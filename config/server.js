658503d712fd417886661debfe246b1972a617d7
Tovy Vaits <antonvaits@gmail.com>
2022-12-12 13:18:05 +0200

    setup server env

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 module.exports = ({ env }) => ({
-  host: env('HOST', 'http://188.120.234.3/'),
+  host: env('HOST', '188.120.234.3'),
   port: env.int('PORT', 1337),
     app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
