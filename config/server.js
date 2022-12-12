6ad967f011f1181f1af3194ecdb416a6b0d58a14
Tovy Vaits <antonvaits@gmail.com>
2022-12-12 11:08:44 +0200

    setting token

--- config/server.js
+++ config/server.js
@@ -4,6 +4,6 @@ module.exports = ({ env }) => ({
     app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
-  url: 'http://188.120.234.3/',
+  // url: 'http://188.120.234.3/',
   proxy: true,
 });
